ff92ea865a94656b242a8005ce6789cc603a657d
GastonGaete <gggaete@uc.cl>
2020-07-16 01:40:26 -0400

    tx p2pkh

---
+++ script.py
@@ -0,0 +1,204 @@
+from io import BytesIO
+from logging import getLogger
+from unittest import TestCase
+
+from helper import (
+    encode_varint,
+    h160_to_p2pkh_address,
+    h160_to_p2sh_address,
+    int_to_little_endian,
+    little_endian_to_int,
+    read_varint,
+)
+from op import (
+    op_equal,
+    op_hash160,
+    op_verify,
+    OP_CODE_FUNCTIONS,
+    OP_CODE_NAMES,
+)
+
+
+def p2pkh_script(h160):
+    '''Takes a hash160 and returns the p2pkh ScriptPubKey'''
+    return Script([0x76, 0xa9, h160, 0x88, 0xac])
+
+
+def p2sh_script(h160):
+    '''Takes a hash160 and returns the p2sh ScriptPubKey'''
+    return Script([0xa9, h160, 0x87])
+
+
+LOGGER = getLogger(__name__)
+
+
+class Script:
+
+    def __init__(self, cmds=None):
+        if cmds is None:
+            self.cmds = []
+        else:
+            self.cmds = cmds
+
+    def __repr__(self):
+        result = []
+        for cmd in self.cmds:
+            if type(cmd) == int:
+                if OP_CODE_NAMES.get(cmd):
+                    name = OP_CODE_NAMES.get(cmd)
+                else:
+                    name = 'OP_[{}]'.format(cmd)
+                result.append(name)
+            else:
+                result.append(cmd.hex())
+        return ' '.join(result)
+
+    def __add__(self, other):
+        return Script(self.cmds + other.cmds)
+
+    @classmethod
+    def parse(cls, s):
+        # get the length of the entire field
+        length = read_varint(s)
+        # initialize the cmds array
+        cmds = []
+        # initialize the number of bytes we've read to 0
+        count = 0
+        # loop until we've read length bytes
+        while count < length:
+            # get the current byte
+            current = s.read(1)
+            # increment the bytes we've read
+            count += 1
+            # convert the current byte to an integer
+            current_byte = current[0]
+            # if the current byte is between 1 and 75 inclusive
+            if current_byte >= 1 and current_byte <= 75:
+                # we have an cmd set n to be the current byte
+                n = current_byte
+                # add the next n bytes as an cmd
+                cmds.append(s.read(n))
+                # increase the count by n
+                count += n
+            elif current_byte == 76:
+                # op_pushdata1
+                data_length = little_endian_to_int(s.read(1))
+                cmds.append(s.read(data_length))
+                count += data_length + 1
+            elif current_byte == 77:
+                # op_pushdata2
+                data_length = little_endian_to_int(s.read(2))
+                cmds.append(s.read(data_length))
+                count += data_length + 2
+            else:
+                # we have an opcode. set the current byte to op_code
+                op_code = current_byte
+                # add the op_code to the list of cmds
+                cmds.append(op_code)
+        if count != length:
+            raise SyntaxError('parsing script failed')
+        return cls(cmds)
+
+    def raw_serialize(self):
+        # initialize what we'll send back
+        result = b''
+        # go through each cmd
+        for cmd in self.cmds:
+            # if the cmd is an integer, it's an opcode
+            if type(cmd) == int:
+                # turn the cmd into a single byte integer using int_to_little_endian
+                result += int_to_little_endian(cmd, 1)
+            else:
+                # otherwise, this is an element
+                # get the length in bytes
+                length = len(cmd)
+                # for large lengths, we have to use a pushdata opcode
+                if length < 75:
+                    # turn the length into a single byte integer
+                    result += int_to_little_endian(length, 1)
+                elif length > 75 and length < 0x100:
+                    # 76 is pushdata1
+                    result += int_to_little_endian(76, 1)
+                    result += int_to_little_endian(length, 1)
+                elif length >= 0x100 and length <= 520:
+                    # 77 is pushdata2
+                    result += int_to_little_endian(77, 1)
+                    result += int_to_little_endian(length, 2)
+                else:
+                    raise ValueError('too long an cmd')
+                result += cmd
+        return result
+
+    def serialize(self):
+        # get the raw serialization (no prepended length)
+        result = self.raw_serialize()
+        # get the length of the whole thing
+        total = len(result)
+        # encode_varint the total length of the result and prepend
+        return encode_varint(total) + result
+
+    def evaluate(self, z):
+        # create a copy as we may need to add to this list if we have a RedeemScript
+        cmds = self.cmds[:]
+        stack = []
+        # Technically, Script has an acces to a second stack via a few commands, but we will not use that here
+        while len(cmds) > 0:
+            cmd = cmds.pop(0)
+            if type(cmd) == int:
+                # The command is an pocode, so do what the opcode says
+                operation = OP_CODE_FUNCTIONS[cmd]
+
+                # The commands that will need the z for checking sig are OP_CHECKSIG and OP_CHECKSIGVERIFY
+                # Their codes are 172 and 173
+                # On https://en.bitcoin.it/wiki/Script you can also check that 174 and 175 also need z
+                if cmd in (172, 173):
+                    # these are signing operations, they need a sig_hash
+                    # to check against
+                    if not operation(stack, z):
+                        LOGGER.info('bad op: {}'.format(OP_CODE_NAMES[cmd]))
+                        return False
+                else:
+                    if not operation(stack):
+                        LOGGER.info('bad op: {}'.format(OP_CODE_NAMES[cmd]))
+                        return False
+            else:
+                # cmd is data, so add the cmd to the stack
+                stack.append(cmd)
+
+        # If we end up with an empty stack the execution failed
+        if len(stack) == 0:
+            return False
+        # If we end up with False on the stack, the execution also failed
+        # Need to tell you about numbers a bit more
+        if stack.pop() == b'':
+            return False
+        return True
+
+    def is_p2pkh_script_pubkey(self):
+        '''Returns whether this follows the
+        OP_DUP OP_HASH160 <20 byte hash> OP_EQUALVERIFY OP_CHECKSIG pattern.'''
+        return len(self.cmds) == 5 and self.cmds[0] == 0x76 \
+            and self.cmds[1] == 0xa9 \
+            and type(self.cmds[2]) == bytes and len(self.cmds[2]) == 20 \
+            and self.cmds[3] == 0x88 and self.cmds[4] == 0xac
+
+    def is_p2sh_script_pubkey(self):
+        '''Returns whether this follows the
+        OP_HASH160 <20 byte hash> OP_EQUAL pattern.'''
+        return len(self.cmds) == 3 and self.cmds[0] == 0xa9 \
+            and type(self.cmds[1]) == bytes and len(self.cmds[1]) == 20 \
+            and self.cmds[2] == 0x87
+
+    def address(self, testnet=False):
+        '''Returns the address corresponding to the script'''
+        if self.is_p2pkh_script_pubkey():  # p2pkh
+            # hash160 is the 3rd cmd
+            h160 = self.cmds[2]
+            # convert to p2pkh address using h160_to_p2pkh_address (remember testnet)
+            return h160_to_p2pkh_address(h160, testnet)
+        elif self.is_p2sh_script_pubkey():  # p2sh
+            # hash160 is the 2nd cmd
+            h160 = self.cmds[1]
+            # convert to p2sh address using h160_to_p2sh_address (remember testnet)
+            return h160_to_p2sh_address(h160, testnet)
+
